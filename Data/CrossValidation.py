41c3e8649534a5433fe6b4a649b561724000b361
Hakapuff <caspernbak@gmail.com>
2024-09-23 15:07:25 +0200

    Test

--- Data/CrossValidation.py
+++ Data/CrossValidation.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
 # Load the labeled features dataset
-labels_features = pd.read_csv('merged_training_dataset.csv')
+labels_features = pd.read_csv('Data/merged_training_dataset.csv')
 
 # labels_features is the labeled dataset
 X = labels_features.drop(['Filename', 'Label'], axis=1)
@@ -20,5 +20,3 @@ print("Accuracy scores for each fold:", cv_scores)
 # Compute the mean and standard deviation of the cross-validation scores
 print("Mean cross-validation accuracy:", cv_scores.mean())
 print("Standard deviation of cross-validation scores:", cv_scores.std())
-
-
