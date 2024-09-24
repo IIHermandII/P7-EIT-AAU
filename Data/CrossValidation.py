3e5c96b18b6a5d43ad1fb22eac3ff325c3f63428
Hakapuff <caspernbak@gmail.com>
2024-09-24 08:38:25 +0200

    Test

--- Data/CrossValidation.py
+++ Data/CrossValidation.py
@@ -6,13 +6,13 @@ import pandas as pd
 labels_features = pd.read_csv('Data/merged_training_dataset.csv')
 
 # labels_features is the labeled dataset
-X = labels_features.drop(['Filename', 'Label'], axis=1)
+x = labels_features.drop(['Filename', 'Label'], axis=1)
 y = labels_features['Label']
 
 rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 
 # Perform x-fold cross-validation
-cv_scores = cross_val_score(rf_classifier, X, y, cv=7, scoring='accuracy')
+cv_scores = cross_val_score(rf_classifier, x, y, cv=7, scoring='accuracy')
 
 # Print the accuracy for each fold
 print("Accuracy scores for each fold:", cv_scores)
@@ -20,3 +20,5 @@ print("Accuracy scores for each fold:", cv_scores)
 # Compute the mean and standard deviation of the cross-validation scores
 print("Mean cross-validation accuracy:", cv_scores.mean())
 print("Standard deviation of cross-validation scores:", cv_scores.std())
+
+
