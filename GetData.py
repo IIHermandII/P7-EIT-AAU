da7a92ed565a57fffc84e361915ff21fb20e326f
Hermand <hermand8320@gmail.com>
2024-09-30 11:19:34 +0200

    Updatet version that splits file perfect

--- GetData.py
+++ GetData.py
@@ -2,11 +2,11 @@ import os
 import sys
 import wave 
 import pylab
+import datetime
 import matplotlib.pyplot as plt
 import numpy as np  
 from scipy import signal
 from scipy.io import wavfile
-
 
 def AudioInfo(wav_file, file1):
     print("--------"+ file1 + " INFO-----------")
@@ -14,7 +14,6 @@ def AudioInfo(wav_file, file1):
     numberOfAudioFrames = wav_file.getnframes()
     SterioOrMono = wav_file.getnchannels()
     print("sampleFrequensy: " + str(sampleFrequensy) + "\nnumberOfAudioFrames: " + str(numberOfAudioFrames) + "\nSterioOrMono: " + str(SterioOrMono))
-
 
 def AudioPlot(wav_file,file1):
     signal = wav_file.readframes(-1)
@@ -48,27 +47,68 @@ def MakeSmaleWavFile(file1):
     newAudio = newAudio[t1:t2]
     newAudio.export('NewData.wav', format="wav") #Exports to a wav file in the current path
 
-
-
-
-
-def main():
+# Uses env var 
+def FindDataDir():
     envP7Path = os.getenv("P7Path")
     if envP7Path is None:
         print("---> If you are working in vscode\n---> you need to restart the aplication\n---> After you have made a env\n---> for vscode to see it!!")
         print("---> You need to make a env called 'P7Path' containing the path to P7 root dir")
         raise ValueError('Envirement Variable not fount (!env)')
-    workDir = envP7Path + "\\Data"
-    RefinedDataDir = workDir + "\\Refined data"
-    os.chdir(RefinedDataDir)
+    workDir = envP7Path + "\\Data\\Refined data\\Labeled data"
+    os.chdir(workDir) # Changes Dir to working Dir ( Labeled data )
     print(os.getcwd())
-    file1 = "2.wav"
+    BreathInDir     = workDir + "\\Breath in"
+    BreathOutDir    = workDir + "\\Breath out"
+    CrossTalkDir    = workDir + "\\Cross talk"
+    VoiceDir        = workDir + "\\Voice"
+    DirList = [BreathInDir, BreathOutDir, CrossTalkDir, VoiceDir]
+    return DirList
 
-    wav_file = wave.open(file1,'r')
-    AudioInfo(wav_file, file1)
-    MakeSmaleWavFile(file1)
-    #AudioPlot(wav_file, file1)
+def MakeWorkingDir(DirList,soundKlipLength):
+    currentTime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    print(currentTime)
+    DataDirList = DirList
+    DataIdentyfire = str(soundKlipLength) + "ms_" + str(currentTime)
+    workDir = os.getcwd() 
+    biDir = workDir + '\\PROCESSED DATA\\bi\\bi_' + DataIdentyfire 
+    boDir = workDir + '\\PROCESSED DATA\\bo\\bo_' + DataIdentyfire
+    ctDir = workDir + '\\PROCESSED DATA\\ct\\ct_' + DataIdentyfire
+    voDir = workDir + '\\PROCESSED DATA\\vo\\vo_' + DataIdentyfire
+    os.makedirs(biDir)
+    os.makedirs(boDir)
+    os.makedirs(ctDir)
+    os.makedirs(voDir)
+    ProcessedDirList = [biDir, boDir, ctDir, voDir]
+    return ProcessedDirList
 
+def SplitData(AudioClipLength, DirList, ProcessedDirList):
+    from pydub import AudioSegment
+    FileNameList = ["2_Breath_in.wav", "2_Breath_out.wav", "2_Cross_talk.wav", "2_Voice.wav"]
+    for i in range(len(ProcessedDirList)):
+        print(os.getcwd())
+        AudioFile = DirList[i] + "\\" + FileNameList[i]
+        print(AudioFile)
+        AudioFile = AudioSegment.from_wav(AudioFile)
+        TotalLengthInMs = len(AudioFile)
+        print("LENGTH : " + str(TotalLengthInMs))
+        t1 = 1
+        t2 = AudioClipLength
+        working = 1
+        while(working):
+            if TotalLengthInMs > t2:
+                NewAudioFile = AudioFile[t1:t2]
+                DestintFile = ProcessedDirList[i] + "\\Data" + str(t1) + "-" + str(t2) + ".wav"
+                NewAudioFile.export(DestintFile, format="wav") #Exports to a wav file in the current path
+            else:
+                working = 0
+            t1 = t2
+            t2 = t2 + AudioClipLength
+
+def main():
+    AudioClipLength = 100 # in ms 
+    DirList = FindDataDir()
+    ProcessedDirList = MakeWorkingDir(DirList, AudioClipLength)
+    SplitData(AudioClipLength, DirList, ProcessedDirList)
 
 if __name__ == "__main__":
     main()
