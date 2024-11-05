c6265ad08d18474351a79884ee19cb3d9b7cc653
MagHawk <mag.hoegh@gmail.com>
2024-11-05 08:52:55 +0100

    Small corrections (for that github score)

--- neural-networks-sklearn.py
+++ neural-networks-sklearn.py
@@ -1,14 +1,11 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import re
-#import seaborn as sns
 import numpy as np
 
 
